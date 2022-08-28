c1c4f4bbb162b6e1314bc800102331cd427a1b0a
Igor Goncalo <igorlabakig@gmail.com>
2022-08-28 22:21:59 +0100

    fix flex wrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,13 +31,44 @@ const Home: NextPage = () => {
           alt="bg-header image"
         />
       </div>
-      <div className=" absolute left-0 right-0 mr-auto ml-auto w-[95%] md:[80%] top-[5rem]  bg-white rounded-lg flex justify-between items-center  px-3 py-2">
+      <div className=" absolute left-0 right-0 mr-auto ml-auto w-[95%] md:[80%] top-[5rem] break-words  bg-white rounded-lg flex flex-col space-y-2 justify-start items-start  px-3 py-2">
         <div className="flex space-x-2 w-[100%] mr-5 break-before-right">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (!search) {
+                setSearch(() => [searchText]);
+              } else {
+                if (search.find((item: string) => item === searchText)) {
+                  return;
+                }
+                setSearch((prevState: any) => [...prevState, searchText]);
+              }
+              setSearchText("");
+            }}
+            className={`flex-1 flex justify-center items-center space-x-2`}
+          >
+            <input
+              type="text"
+              className="bg-LightGrayishCyan outline-none rounded-md w-[100%] px-2 h-6 text-sm font-semibold text-desaturatedDarkCyan"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+
+            <p
+              onClick={() => setSearch(() => [])}
+              className="cursor-pointer font-bold text-[12px] text-desaturatedDarkCyan hover:underline"
+            >
+              Clear
+            </p>
+          </form>
+        </div>
+        <div className="bg-white flex flex-wrap gap-y-2 gap-x-3 justify-start items-start">
           {search?.map((skill: any, i: number) => {
             return (
               <div
                 key={i}
-                className={`bg-LightGrayishCyan text-sm font-semibold h-6 overflow-hidden text-desaturatedDarkCyan flex rounded-sm`}
+                className={`bg-LightGrayishCyan text-sm  font-semibold h-6 overflow-hidden text-desaturatedDarkCyan flex rounded-sm`}
               >
                 <p className="px-1 text-[13px] font-bold">{skill}</p>
                 <div className="bg-desaturatedDarkCyan hover:bg-veryDarkGraishCyan w-[20px] cursor-pointer flex justify-center items-center">
@@ -54,37 +85,13 @@ const Home: NextPage = () => {
               </div>
             );
           })}
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (!search) {
-                setSearch(() => [searchText]);
-              } else {
-                if (search.find((item: string) => item === searchText)) {
-                  return;
-                }
-                setSearch((prevState: any) => [...prevState, searchText]);
-              }
-              setSearchText("");
-            }}
-            className={`flex-1`}
-          >
-            <input
-              type="text"
-              className="bg-LightGrayishCyan outline-none rounded-md w-[100%] px-2 h-6 text-sm font-semibold text-desaturatedDarkCyan"
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-            />
-          </form>
         </div>
-        <p
-          onClick={() => setSearch(() => [])}
-          className="cursor-pointer font-bold text-[12px] text-desaturatedDarkCyan hover:underline"
-        >
-          Clear
-        </p>
       </div>
-      <div className="flex flex-col  flex-1 min-h-screen space-y-10 py-5 w-[95%] md:[80%] m-auto my-9">
+      <div
+        className={`flex flex-col flex-1 min-h-screen space-y-10 py-5 w-[95%] md:[80%] m-auto my-12 md:my-8 ${
+          search.length > 0 ? "my-16" : null
+        } `}
+      >
         {filterNoResults.map((job: any, i) => {
           return (
             <JobOportunity
